b9ab62667aba01cf99d4dc3caf48d4111ac5776a
Ashley <a.clarkem@gmail.com>
2014-09-04 15:12:18 -0600

    secure password working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/register' => 'users#new', as: :users
+  post '/register' => 'users#create'
 end
