ba8c766b573d547db3cec77149d6437f06268ec5
Ashley <a.clarkem@gmail.com>
2014-09-04 16:50:14 -0600

    created header for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/welcome' => 'welcome#index'
   get '/register' => 'registrations#new', as: :users
   post '/register' => 'registrations#create'
+  get '/signin' => 'sessions#new', as: :signin
+  post '/signin' => 'sessions#create'
 end
